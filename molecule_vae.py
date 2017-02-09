37a3346825ba9ca714204733aa428f58d6bbed3d
Brooks <brooks@robots.ox.ac.uk>
2017-02-09 19:19:53 +0000

    Added friendly wrapper-class for the grammar VAE.

---
+++ molecule_vae.py
@@ -0,0 +1,117 @@
+import nltk
+import numpy as np
+
+import grammar.zinc_grammar
+import molecules.model_zinc
+
+def get_zinc_tokenizer(cfg):
+    long_tokens = filter(lambda a: len(a) > 1, cfg._lexical_index.keys())
+    replacements = ['$','%','^','&']
+    assert len(long_tokens) == len(replacements)
+    for token in replacements: 
+        assert not cfg._lexical_index.has_key(token)
+    
+    def tokenize(smiles):
+        for i, token in enumerate(long_tokens):
+            smiles = smiles.replace(token, replacements[i])
+        tokens = []
+        for token in smiles:
+            try:
+                ix = replacements.index(token)
+                tokens.append(long_tokens[ix])
+            except:
+                tokens.append(token)
+        return tokens
+    
+    return tokenize
+
+def pop_or_nothing(S):
+    try: return S.pop()
+    except: return 'Nothing'
+
+def prods_to_eq(prods):
+    seq = [prods[0].lhs()]
+    for prod in prods:
+        if str(prod.lhs()) == 'Nothing':
+            break
+        for ix, s in enumerate(seq):
+            if s == prod.lhs():
+                seq = seq[:ix] + list(prod.rhs()) + seq[ix+1:]
+                break
+    return ''.join(seq)
+
+
+
+class ZincGrammarModel(object):
+
+    def __init__(self, weights_file, latent_rep_size=56):
+        """ Load the (trained) zinc encoder/decoder, grammar model. """
+        self._grammar = grammar.zinc_grammar
+        self._model = molecules.model_zinc
+        self.MAX_LEN = self._model.MAX_LEN
+        self._productions = self._grammar.GCFG.productions()
+        self._prod_map = {}
+        for ix, prod in enumerate(self._productions):
+            self._prod_map[prod] = ix
+        self._parser = nltk.ChartParser(self._grammar.GCFG)
+        self._tokenize = get_zinc_tokenizer(self._grammar.GCFG)
+        self._n_chars = len(self._productions)
+        self._lhs_map = {}
+        for ix, lhs in enumerate(self._grammar.lhs_list):
+            self._lhs_map[lhs] = ix
+        self.vae = self._model.MoleculeVAE()
+        self.vae.load(self._productions, weights_file, max_length=self.MAX_LEN, latent_rep_size=latent_rep_size)
+
+    def encode(self, smiles):
+        """ Encode a list of smiles strings into the latent space """
+        assert type(smiles) == list
+        tokens = map(self._tokenize, smiles)
+        parse_trees = [self._parser.parse(t).next() for t in tokens]
+        productions_seq = [tree.productions() for tree in parse_trees]
+        indices = [np.array([self._prod_map[prod] for prod in entry], dtype=int) for entry in productions_seq]
+        one_hot = np.zeros((len(indices), self.MAX_LEN, self._n_chars), dtype=np.float32)
+        for i in xrange(len(indices)):
+            num_productions = len(indices[i])
+            one_hot[i][np.arange(num_productions),indices[i]] = 1.
+            one_hot[i][np.arange(num_productions, self.MAX_LEN),-1] = 1.
+        return self.vae.encoderMV.predict(one_hot)[0]
+
+    def _sample_using_masks(self, unmasked):
+        """ Samples a one-hot vector, masking at each timestep """
+        eps = 1e-100
+    #     ln_p = np.zeros((unmasked.shape[0],))
+        X_hat = np.zeros_like(unmasked)
+
+        # Create a stack for each input in the batch
+        S = np.empty((unmasked.shape[0],), dtype=object)
+        for ix in xrange(S.shape[0]):
+            S[ix] = [str(self._grammar.start_index)]
+
+        # Loop over time axis, sampling values and updating masks
+        for t in xrange(unmasked.shape[1]):
+            next_nonterminal = [self._lhs_map[pop_or_nothing(a)] for a in S]
+            mask = self._grammar.masks[next_nonterminal]
+            masked_output = np.exp(unmasked[:,t,:])*mask + eps
+            sampled_output = np.argmax(np.random.gumbel(size=masked_output.shape) + np.log(masked_output), axis=-1)
+            X_hat[np.arange(unmasked.shape[0]),t,sampled_output] = 1.0
+    #         prob = masked_output / np.sum(masked_output, axis=-1, keepdims=True)
+    #         ln_p += np.log((prob*X_hat[:,t,:]).sum(-1))
+
+            # Identify non-terminals in RHS of selected production, and
+            # push them onto the stack in reverse order
+            rhs = [filter(lambda a: (type(a) == nltk.grammar.Nonterminal) and (str(a) != 'None'),
+                          self._productions[i].rhs()) 
+                   for i in sampled_output]
+            for ix in xrange(S.shape[0]):
+                S[ix].extend(map(str, rhs[ix])[::-1])
+        return X_hat # , ln_p
+
+    def decode(self, z):
+        assert z.ndim == 2
+        unmasked = self.vae.decoder.predict(z)
+        X_hat = self._sample_using_masks(unmasked)
+        # Convert from one-hot to sequence of production rules
+        prod_seq = [[self._productions[X_hat[index,t].argmax()] 
+                     for t in xrange(X_hat.shape[1])] 
+                    for index in xrange(X_hat.shape[0])]
+        return [prods_to_eq(prods) for prods in prod_seq]
