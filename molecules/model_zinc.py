93269d8974b32763950c8e309105b3fb87b7db6e
Brooks <brooks@robots.ox.ac.uk>
2017-02-09 19:06:43 +0000

    Made "grammar" into its own module

--- molecules/model_zinc.py
+++ molecules/model_zinc.py
@@ -10,7 +10,7 @@ from keras.layers.convolutional import Convolution1D
 #from terminalgru import TerminalGRU
 #from recurrentshop import*
 import tensorflow as tf
-import zinc_grammar as G
+import grammar.zinc_grammar as G
 
 # helper variables in Keras format for parsing the grammar
 masks_K      = K.variable(G.masks)
