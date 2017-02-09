b6952a0aa65a1929f05035b4aaf295ccf9f5c573
mkusner <matt.kusner@gmail.com>
2017-02-09 13:30:32 +0000

    models

--- molecules/model_zinc.py
+++ molecules/model_zinc.py
@@ -7,7 +7,18 @@ from keras.layers.core import Dense, Activation, Flatten, RepeatVector
 from keras.layers.wrappers import TimeDistributed
 from keras.layers.recurrent import GRU
 from keras.layers.convolutional import Convolution1D
-import pdb
+#from terminalgru import TerminalGRU
+#from recurrentshop import*
+import tensorflow as tf
+import zinc_grammar as G
+
+# helper variables in Keras format for parsing the grammar
+masks_K      = K.variable(G.masks)
+ind_of_ind_K = K.variable(G.ind_of_ind)
+
+MAX_LEN = 277
+DIM = G.D
+
 
 class MoleculeVAE():
 
@@ -15,8 +26,8 @@ class MoleculeVAE():
     
     def create(self,
                charset,
-               max_length = 120,
-               latent_rep_size = 292,
+               max_length = MAX_LEN,
+               latent_rep_size = 2,
                weights_file = None):
         charset_length = len(charset)
         
@@ -51,6 +62,7 @@ class MoleculeVAE():
         x2 = Input(shape=(max_length, charset_length))
         (z_m, z_l_v) = self._encoderMeanVar(x2, latent_rep_size, max_length)
         self.encoderMV = Model(input=x2, output=[z_m, z_l_v])
+
         if weights_file:
             self.autoencoder.load_weights(weights_file)
             self.encoder.load_weights(weights_file, by_name = True)
@@ -60,6 +72,7 @@ class MoleculeVAE():
         self.autoencoder.compile(optimizer = 'Adam',
                                  loss = vae_loss,
                                  metrics = ['accuracy'])
+
 
     def _encoderMeanVar(self, x, latent_rep_size, max_length, epsilon_std = 0.01):
         h = Convolution1D(9, 9, activation = 'relu', name='conv_1')(x) # used to be 2, 1
@@ -75,11 +88,11 @@ class MoleculeVAE():
 
 
     def _buildEncoder(self, x, latent_rep_size, max_length, epsilon_std = 0.01):
-        h = Convolution1D(9, 9, activation = 'relu', name='conv_1')(x)
-        h = Convolution1D(9, 9, activation = 'relu', name='conv_2')(h)
-        h = Convolution1D(10, 11, activation = 'relu', name='conv_3')(h)
+        h = Convolution1D(9, 9, activation = 'relu', name='conv_1')(x) # used to be 2, 1
+        h = Convolution1D(9, 9, activation = 'relu', name='conv_2')(h) # used to be 2, 1
+        h = Convolution1D(10, 11, activation = 'relu', name='conv_3')(h) # used to be 3
         h = Flatten(name='flatten_1')(h)
-        h = Dense(435, activation = 'relu', name='dense_1')(h)
+        h = Dense(435, activation = 'relu', name='dense_1')(h) # used to be 100, 30, 50, 200
 
         def sampling(args):
             z_mean_, z_log_var_ = args
@@ -90,7 +103,22 @@ class MoleculeVAE():
         z_mean = Dense(latent_rep_size, name='z_mean', activation = 'linear')(h)
         z_log_var = Dense(latent_rep_size, name='z_log_var', activation = 'linear')(h)
 
+        # this function is the main change.
+        # essentially we mask the training data so that we are only allowed to apply
+        #   future rules based on the current non-terminal
+        def conditional(x_true, x_pred):
+            most_likely = K.argmax(x_true)
+            most_likely = tf.reshape(most_likely,[-1]) # flatten most_likely
+            ix2 = tf.expand_dims(tf.gather(ind_of_ind_K, most_likely),1) # index ind_of_ind with res
+            ix2 = tf.cast(ix2, tf.int32) # cast indices as ints 
+            M2 = tf.gather_nd(masks_K, ix2) # get slices of masks_K with indices
+            M3 = tf.reshape(M2, [-1,MAX_LEN,DIM])#K.int_shape(x_pred)) # reshape them
+            P2 = tf.mul(K.exp(x_pred),M3) # apply them to the exp-predictions
+            P2 = tf.div(P2,K.sum(P2,axis=-1,keepdims=True)) # normalize predictions
+            return P2
+
         def vae_loss(x, x_decoded_mean):
+            x_decoded_mean = conditional(x, x_decoded_mean) # we add this new function to the loss
             x = K.flatten(x)
             x_decoded_mean = K.flatten(x_decoded_mean)
             xent_loss = max_length * objectives.binary_crossentropy(x, x_decoded_mean)
@@ -104,11 +132,20 @@ class MoleculeVAE():
         h = RepeatVector(max_length, name='repeat_vector')(h)
         h = GRU(501, return_sequences = True, name='gru_1')(h)
         h = GRU(501, return_sequences = True, name='gru_2')(h)
-        h = GRU(501, return_sequences = True, name='gru_3')(h)
-        return TimeDistributed(Dense(charset_length, activation='softmax'), name='decoded_mean')(h)
+        h = GRU(501, return_sequences = True, name='gru_3')(h) # all above used to be 100, 30, 50, 200
+        return TimeDistributed(Dense(charset_length), name='decoded_mean')(h) # don't do softmax, we do this in the loss now
+
+        #h = SpecialLayer(charset_length, activation='softmax'), name='decoded_mean')(h)
+        ####rc = RecurrentContainer(readout=True) #, unroll=True, input_length=max_length)
+        ####rc.add(GRUCell(50, name='gru_1', input_shape=((latent_rep_size,)))) #((None,max_length,latent_rep_size)))) # input_shape=((latent_rep_size,max_length))))#input_shape=K.int_shape(h)))
+        ####rc.add(GRUCell(50, name='gru_2'))
+        ####rc.add(GRUCell(50, name='gru_3'))
+        ####rc.add(GRUCell(charset_length, activation='softmax', name='decoded_mean'))
+        ####return rc(h)
+        #return TerminalGRU(charset_length, return_sequences=True, activation='softmax', name='decoded_mean')(h) # NEW!
 
     def save(self, filename):
         self.autoencoder.save_weights(filename)
     
-    def load(self, charset, weights_file, latent_rep_size = 292, max_length = 120):
-        self.create(charset, weights_file = weights_file, latent_rep_size = latent_rep_size)
+    def load(self, charset, weights_file, latent_rep_size = 2, max_length=MAX_LEN):
+        self.create(charset, max_length = max_length, weights_file = weights_file, latent_rep_size = latent_rep_size)
